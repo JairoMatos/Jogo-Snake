3b2030992e26bb9345d66cb845c465a7c062ad08
JairoMatos <jairo.oliveira22@gmail.com>
2023-09-30 14:31:06 -0300

    melhorias

--- script.js
+++ script.js
@@ -23,13 +23,8 @@ velY = 0;
 grid = (canvas.width/100)*5
 //tamanho inicial da cobra
 tam = 5; 
-//dificultade do jogo
 
-
-
-//funcao chamando jogo a cada 100ms
-
-
+//comando botao virtual
 left.addEventListener('click', ()=>{
     velX = -1;
     velY = 0;
@@ -46,7 +41,7 @@ down.addEventListener('click', ()=>{
     velX = 0;
     velY = 1;
 })
-//coonfigurar movimentos da cobra
+//comandos teclado
 document.addEventListener('keydown', (e)=>{
     
     switch(e.key){
@@ -69,21 +64,21 @@ document.addEventListener('keydown', (e)=>{
     }
 }) 
 
+//funcao chamando jogo a cada 100ms
 setInterval(jogo, 200);
-}//fim onload
 
+
+}//fim onload
+ 
+//funcao do game
 const jogo = () =>{
     let tmScore = 0
-    
+ 
     //configuracao da tela
-    ctx.fillStyle = '#008080';
+    
+    ctx.fillStyle = '#E6E6FA';
     //distancia borda H distancia borda V largura alturar
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //gameouver
-    
-   
-   
-   
     //posicionameto da cobra
     snake.push({
         x: posX,    
@@ -91,24 +86,24 @@ const jogo = () =>{
     })
     posX += velX;
     posY += velY;
-   
-    //posicionameto da comida
-    
     
     //configuracao da cobra 
-    ctx.fillStyle = '#8A2BE2'
+    ctx.fillStyle = '#663399'
     for(let i=0; i<snake.length; i++){
         ctx.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1)
         if(snake[i].x == posX && snake[i].y == posY){
             tam = 2
             ctx.fillStyle = 'red';
             score.innerHTML = `SCORE: ${tmScore}`
+            
         }
     } 
     
     while(snake.length > tam){
         snake.shift()
     }
+    
+    //comida
     ctx.fillStyle = 'yellow';
     ctx.fillRect(foodX*grid, foodY*grid, grid-1, grid-1);
 
@@ -119,6 +114,8 @@ const jogo = () =>{
         foodY = Math.floor(Math.random()*20)
         score.innerHTML = `SCORE: ${tam-2}` 
     } 
+    //configurando saida da cobra na tela
+
     if(posX < 0 ){  
         posX = 20; 
     }else if(posX > 20){
@@ -129,4 +126,5 @@ const jogo = () =>{
     }else if(posY > 20){
         posY = 0
     }
+    
 } 
